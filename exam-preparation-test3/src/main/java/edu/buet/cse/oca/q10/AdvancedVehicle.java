f6e7d429efcb9f04fbdcaacc3297dd4d5f29187b
Shamim Ahmed <shamim.ahmed@sandstone.com.au>
2018-11-13 22:55:57 +1100

    rename a class

--- exam-preparation-test3/src/main/java/edu/buet/cse/oca/q10/AdvancedVehicle.java
+++ exam-preparation-test3/src/main/java/edu/buet/cse/oca/q10/AdvancedVehicle.java
@@ -1,6 +1,6 @@
 package edu.buet.cse.oca.q10;
 
-public interface AdvancedVenicle extends Vehicle {
+public interface AdvancedVehicle extends Vehicle {
   @Override
   default String getDescription() {
     return Vehicle.super.getDescription(); // invoking the default method in super interface
